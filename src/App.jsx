7ff00829e6ea789ba633708bbe6451b7deb01e5a
Yuber-X <yubersantanalizardo@gmail.com>
2025-04-15 22:21:04 -0400

    My-Portfolio v2.1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import './App.css';
+import './index.css';
 import { Navbar } from '../src/components/NavBar.jsx';
+import { LoadingScreen } from './components/LoadingScreen.jsx';
+import { MobileMenu } from './components/MobileMenu.jsx';
+import { useState } from 'react';
 
 
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
+
     <div className="App">
-      <Navbar/>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}
+        <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}></div>
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+
     </div>
+
   )
 }
 
--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,7 +1,26 @@
 import { useState,  useEffect } from "react";
 
-export const LodiangScreen = () =>{
+export const LoadingScreen = ({ onComplete }) =>{
+    const [text, setText] = useState("");
+    const fullText = "<Hello World />";
 
+    useEffect (() => {
+        let index = 0;
+        const interval = setInterval(() => {
+            setText(fullText.substring(0, index));
+            index++;
+
+            if(index > fullText.length){
+                clearInterval(interval);
+
+                setTimeout(() => {
+                    onComplete();
+                }, 1000);
+            }
+        }, 100);
+
+        return () => clearInterval(interval);
+    }, [onComplete]);
 
 
 
@@ -11,8 +30,9 @@ export const LodiangScreen = () =>{
                 {text} <span className="animate-blink m1-1"> | </span>
             </div>
 
-            <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden "></div>
-
+            <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
+                <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div> 
+            </div>
         </div>
     );
-}
+};
